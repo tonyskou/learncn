460ab7d01c547bd3d0c211c5ed49c9ba78a5a5f6
tonyskou <40710582+tonyskou@users.noreply.github.com>
2026-07-12 23:47:53 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+var CACHE = 'diabazo-cn-v2';
+var FILES = [
+  './',
+  './index.html',
+  './words.js',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){ return r || fetch(e.request); })
+  );
+});
